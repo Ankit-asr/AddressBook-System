5892dcbfdefa12f34594d4dbd51782f0654a4f53
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-09-07 15:26:13 +0530

    [Ankit] Add: Data inserted into Table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -3,7 +3,7 @@ Create Database AddressBookService
 --UC2 Create Table
 Create TABLE Address_Book(
 FirstName varchar(100),
-SecondName varchar(100),
+LastName varchar(100),
 Address varchar(250),
 City varchar(100),
 State varchar(100),
@@ -11,3 +11,15 @@ Zip BigInt,
 PhoneNumber BigInt,
 EmailAddress varchar(200))
 select *from Address_Book
+
+--UC3 Insert Contacts To AddressBook
+INSERT INTO Address_Book(FirstName,LastName,Address,City,State,Zip,PhoneNumber,EmailAddress) 
+values
+('Ankit','Rathore','Indiranagar','Lucknow','Uttar Pradesh',226953,9696992265,'asingh@gmail.com'),
+('Karan','Singh','Park Road','Bangalore','Karnataka',169855,9901001152,'karan@gmail.com'),
+('Riya','Verma','Avenue Road','Sitapur','Uttar Pradesh',226045,9450172185,'riya45@gmail.com'),
+('Shreya','Shrivastav','Stadium Road','Noida','Uttar Pradesh',226001,9450136987,'shreyash12@gmail.com'),
+('Gopal','Joshi','Kalyanpur','Deheradun','Uttarakhand',562348,9696335519,'gopala@gmail.com');
+
+Select * from Address_Book
+
